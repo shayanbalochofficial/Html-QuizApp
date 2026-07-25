cd8b55bda9cff670cb0c96f19001686893df3588
Shayan Baloch <shayanbalochofficial@gmail.com>
2026-07-26 04:52:52 +0500

    Created Questions for Quiz // Created Myself

--- script.js
+++ script.js
@@ -13,3 +13,51 @@ const maxScoreSpan = document.getElementById("max-score");
 const resultMessage = document.getElementById("result-message");
 const restartButton = document.getElementById("restart-btn");
 const progressBar = document.getElementById("progress");
+
+const quizQuestions = [
+  {
+    question: "What is the capital of Germany?",
+    answers: [
+      { text: "Karachi", correct: false },
+      { text: "Berlin", correct: true },
+      { text: "Dusseldorf", correct: false },
+      { text: "Hamburg", correct: false },
+    ],
+  },
+  {
+    question: "How many states are there in Germany?",
+    answers: [
+      { text: "16", correct: true },
+      { text: "17", correct: false },
+      { text: "18", correct: false },
+      { text: "19", correct: false },
+    ],
+  },
+  {
+    question: "Which country has a border with Germany?",
+    answers: [
+      { text: "Italy", correct: false },
+      { text: "Spain", correct: false },
+      { text: "Poland", correct: true },
+      { text: "Scotland", correct: false },
+    ],
+  },
+  {
+    question: "Which of these is NOT a programming language?",
+    answers: [
+      { text: "Java", correct: false },
+      { text: "Python", correct: false },
+      { text: "Banana", correct: true },
+      { text: "JavaScript", correct: false },
+    ],
+  },
+  {
+    question: "In what year did the Berlin Wall fall??",
+    answers: [
+      { text: "1988", correct: false },
+      { text: "1990", correct: false },
+      { text: "1989", correct: true },
+      { text: "1991", correct: false },
+    ],
+  },
+];
